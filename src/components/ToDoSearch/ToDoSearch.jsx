bc07dc11e12519323cbca71e244b07ee8e91e432
david <davidjuradoyt@gmail.com>
2023-05-26 09:19:44 -0500

    epa

--- src/components/ToDoSearch/ToDoSearch.jsx
+++ src/components/ToDoSearch/ToDoSearch.jsx
@@ -2,23 +2,25 @@ import React from "react"
 import styles from "./todosearch.module.css"
 import { ToDoContext } from "../../context";
 
-const searchIcon = (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className={styles.searchIcon}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-    </svg>
-)
+const SearchIcon = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className={styles.searchIcon}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+        </svg>
+    )
+}
 function ToDoSearch() {
-    const { searchTerm, setSearchTerm} = React.useContext(ToDoContext)
+    const { searchTerm, setSearchTerm } = React.useContext(ToDoContext)
 
     return (
         <div className={styles.todoInputWrapper}>
             <input
-                value = {searchTerm}
+                value={searchTerm}
                 placeholder="Search a task"
                 className={styles.todoInput}
-                onChange={(e)=>setSearchTerm(e.target.value)}
+                onChange={(e) => setSearchTerm(e.target.value)}
             />
-            <span className={styles.todoInputButton}>{searchIcon}</span>
+            <span className={styles.todoInputButton}><SearchIcon/></span>
         </div>
     );
 }
